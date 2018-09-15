041a4a9bb0f19b388e0d71ded65664b88d2c0c94
zhmouohz <zhmouohz@163.com>
2018-09-15 17:10:05 +0800

    打包

--- pm-server/Windowservice.js
+++ pm-server/Windowservice.js
@@ -10,7 +10,6 @@ var svc = new Service({
 // Listen for the "install" event, which indicates the
 // process is available as a service.
 svc.on('install', function() {
-  console.log('install')
   svc.start()
 })
 svc.install()
